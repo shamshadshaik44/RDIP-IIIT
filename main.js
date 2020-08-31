9aff9b1d14ef13c2d670b895192ea3e880450e7c
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-01 00:18:15 +0530

    added two variables and printed using console log

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-document.write("JavaScript is a full-fledged dynamic programming language that can add interactivity to a website. was invented by Brendan Eich (co-founder of the Mozilla project), the Mozilla Foundation,and the Mozilla Corporation.JavaScript is versatile and beginner-friendly. With more experience, you ll be able to create games, animated 2D and 3D graphics, comprehensive database-driven apps, and much more!JavaScript itself is relatively compact, yet very flexible. Developers have written a variety of tools on top of the core JavaScript language, unlocking a vast amount of functionality with minimum effort. These include:Browser Application Programming Interfaces (APIs) built into web browsers, providing functionality such as dynamically creating HTML and setting CSS styles; collecting and manipulating a video stream from a user's webcam, or generating 3D graphics and audio samples.Third-party APIs that allow developers to incorporate functionality in sites from other content providers, such as Twitter or Facebook.");
+var x=5;
+var ch='a'
+console.log(x);
+console.log(ch);
