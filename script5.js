ba3369cd4732a0522cf88cdfae976971e8a8dd13
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-02 02:59:07 +0530

    used conditional case whether given element is betwen 10 and 100 and fibonscii using for loop

--- script5.js
+++ script5.js
@@ -1,6 +1,6 @@
 /* var dat=prompt(" date:");
 var d=new Date(dat);
-console.log(d); */
+console.log(d); 
 function myFunction() {
   var x = document.getElementById("myInput").value;
   document.getElementById("res").innerHTML = "You wrote: " + x;
@@ -8,4 +8,25 @@ function myFunction() {
 function myFunction1() {
   var x = document.getElementById("myInput");
  document.getElementById("res2").innerHTML = x.value.toUpperCase();
+}*/
+var num=prompt("enter");
+if(Number(num)>10 &&Number(num)<100)
+{
+	console.log('yes');
+}
+else
+{
+	alert("Error:number is not between 10 and 100");
+}
+var num1=0;
+var num2=1;
+var next;
+var n=prompt("enter");
+for(i=1;i<=n;i++)
+{ 
+	document.writeln(num1);
+	next=num1+num2;
+	num1=num2;
+	num2=next;
+    
 }
