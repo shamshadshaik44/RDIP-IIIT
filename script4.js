b6409bdb2f341adb832b158dd6ea77b40263b24c
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-01 16:28:10 +0530

    name contains only alphabets using regexp

--- script4.js
+++ script4.js
@@ -1,5 +1,9 @@
-function multiply(a,b)
+/*function multiply(a,b)
 {
 	return a*b;
 }
-document.getElementById("result").innerHTML=multiply(5,6);
+ document.getElementById("result").innerHTML=multiply(5,6); */
+var str="introP566";
+var regex=/[a-z A-Z]/g;
+var res=str.match(regex);
+document.getElementById("result").innerHTML=res;
