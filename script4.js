b6e83cb69c47d1dad0195efc08107211f7cc1353
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-01 16:43:53 +0530

    used predefined math functions

--- script4.js
+++ script4.js
@@ -3,7 +3,15 @@
 	return a*b;
 }
  document.getElementById("result").innerHTML=multiply(5,6); */
-var str="introP566";
+/*var str="introP566";
 var regex=/[a-z A-Z]/g;
 var res=str.match(regex);
-document.getElementById("result").innerHTML=res;
+document.getElementById("result").innerHTML=res; */
+document.writeln(Math.pow(5,2));
+document.writeln(Math.sqrt(256));
+document.writeln(Math.abs(-35.27));
+document.writeln(Math.ceil(35.17));
+document.writeln(Math.floor(35.17));
+document.writeln(Math.cos(30));
+document.writeln(Math.sin(30));
+document.writeln(Math.tan(30));
