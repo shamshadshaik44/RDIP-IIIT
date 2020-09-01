7cc0a2c3870f67cb3f228f7c162e2e28c41ee56d
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-02 02:23:20 +0530

    practising on events

--- script5.js
+++ script5.js
@@ -1,3 +1,11 @@
-var dat=prompt(" date:");
+/* var dat=prompt(" date:");
 var d=new Date(dat);
-console.log(d);
+console.log(d); */
+function myFunction() {
+  var x = document.getElementById("myInput").value;
+  document.getElementById("res").innerHTML = "You wrote: " + x;
+}
+function myFunction1() {
+  var x = document.getElementById("myInput");
+ document.getElementById("res2").innerHTML = x.value.toUpperCase();
+}
