707cd4c1d88f848240a44c5b5be26694ec966892
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-01 19:24:50 +0530

    validation form

--- script4.js
+++ script4.js
@@ -6,7 +6,7 @@
 /*var str="introP566";
 var regex=/[a-z A-Z]/g;
 var res=str.match(regex);
-document.getElementById("result").innerHTML=res; */
+document.getElementById("result").innerHTML=res;
 document.writeln(Math.pow(5,2));
 document.writeln(Math.sqrt(256));
 document.writeln(Math.abs(-35.27));
@@ -14,4 +14,26 @@ document.writeln(Math.ceil(35.17));
 document.writeln(Math.floor(35.17));
 document.writeln(Math.cos(30));
 document.writeln(Math.sin(30));
-document.writeln(Math.tan(30));
+document.writeln(Math.tan(30)); */
+function validnum(phone,alertMsg){
+	var num=/^[0-9]+$/;
+	if(phone.value.match(num)){
+		return true;
+	}
+	else{
+		document.getElementById('phone').innerHTML=alertMsg;
+		return false;
+	}
+	}
+
+function validateForm()
+{
+	var x=document.forms["myfrom"]["reason"].value;
+	if(x==""){
+		alert("resaon must be filled out");
+		return false;
+	}
+}
+
+
+
