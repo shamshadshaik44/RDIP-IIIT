629629978835555edee857d1a496cdb5cffc6424
shamshadshaik44 <shamshadshaik44@gmail.com>
2020-09-04 19:05:10 +0530

    jquery get and post methods

--- bootstrap.js
+++ bootstrap.js
@@ -4,13 +4,36 @@
 	{
 		$("p").toggle();
 	});
-}); */
+}); 
 $(document).ready(function()
 {
 	$("button").click(function()
 	{
 	   $("#1").fadeToggle();
 		$("#2").fadeToggle("slow");
+	});
+});
+*/
+$(document).ready(function(){
+	$("#1").click(function(){
+		$.get("https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS",
+			function(data,status){
+				alert("data:"+data+"\nstatus:"+status);
+			});
+			
+	});
+});
+$(document).ready(function(){
+	$("#2").click(function(){
+		$.post("https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS",
+		{
+			name:"shamshad",
+			branch:"EEE"
+
+		},
+		function(data,status){
+			alert("data: "+data+"\nstatus:"+status);
+		});
 	});
 });
 
